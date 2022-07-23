4668f340d2025830d09379095357cfd453c44d1d
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-07-23 07:29:34 +0000

    [finish]admin/ヘッダー作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,16 +1,17 @@
 class Admin::GenresController < ApplicationController
   def index
   end
-  
+
   def create
   end
-  
+
   def update
   end
-  
+
   def show
   end
-  
+
   def destroy
-    
+  end
+
 end

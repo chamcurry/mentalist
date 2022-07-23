587113ce277ef5789e029297af5db08a64ebd70c
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-07-23 13:46:35 +0000

    [update]SQLite

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,10 @@
 require_relative 'boot'
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+# require "active_storage/engine"
 module Mentalist
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.

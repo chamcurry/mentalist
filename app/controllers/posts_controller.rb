a0a63c3a27d5649c5f886a5152f31d5e6a9e9139
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-07-26 03:45:22 +0000

    [midway]会員/新規投稿

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
   end
 
   def show
@@ -16,5 +21,10 @@ class PostsController < ApplicationController
     @keyword = params[:keyword]
   end
   def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:body,:star,:image)
   end
 end

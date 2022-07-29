f666a9cd90c83b794e0f42203a42d56ce7a293d7
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-07-29 17:32:23 +0000

    [create]退会機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_sign_in_params, only: [:create]
+  protected
+  # 退会しているか判断するためのメソッド
+  def configure_sign_in_params
+    # [処理内容１]入力されたemailからアカウントを1件取得
+    @menber = Menber.find_by(email: params[:menber][:email])
+    # アカウントが取得できない場合、このメソッドを終了する
+    return if !@menber
+    # [処理内容２]取得したアカウントのパスワードと入力されたパスワードが一致しているか判別
+    if @menber.valid_password?(params[:menber][:password]) && @menber.is_active == true
+      # [処理内容３]is_activeの値がtrueだった場合、サインアップ画面に遷移させる
+       redirect_to new_menber_registration_path
+    end
+  end
 end

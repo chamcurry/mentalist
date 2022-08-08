63dd8cc3d73feaced7a1e367cbfc3865478f00c8
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-08-08 18:25:52 +0000

    [patch]検索機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,10 @@
 class PostsController < ApplicationController
   # ゲストユーザーとしてログインした場合は閲覧を制限する
   before_action :guest_check,except: [:show]
-  
+
   def index
-    # 該当するジャンルを検索。なければ全てを表示する
-    @genres = params[:genre_id].present? ? Genre.find(params[:genre_id]).posts: Post.all
-    @posts=  Post.all.page(params[:page]).per(5).search(params[:search])
-
-    # genre_search = Genre.where("soup = ?",params[:soup]).pluck(:id)
-    # key_search = Post.where("title LIKE (?)","#{params[:title]}").pluck(:id)
-    # @submit_searh = Post.where("genre_id IN (?) or title IN (?)",key_search,genre_search)
-    # @posts = Post.all
-
+    @posts= Post.search(params[:search])
+    @genres=Genre.genre_search(params[:search])
   end
   def new
     @post = Post.new
@@ -29,10 +22,6 @@ class PostsController < ApplicationController
   def show
   end
 
-  def search
-    @posts = Post.search(params:[:keyword])
-    @keyword = params[:keyword]
-  end
   def destroy
   end
 
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,12 +2,11 @@ class Genre < ApplicationRecord
   has_many :post_genres,dependent: :destroy
   has_many :posts,through: :post_genres,dependent: :destroy
 
-  # def self.search(search)
-  #   if search
-  #     genre= Genre.where(soup: search)
-  #     genre[0].submits
-  #   else
-  #     Post.all
-  #   end
-  # end
+  def self.genre_search(search)
+    if search
+      Genre.where(["soup like?","%#{search}%"])
+    else
+      Post.all
+    end
+  end
 end

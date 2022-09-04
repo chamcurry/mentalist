4f2a69fa327d2f6ef58f1ee1d1a7e6c7fdfedb15
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-09-04 08:26:36 +0000

    [update/いいね一覧]検索機能対応

--- app/controllers/menbers_controller.rb
+++ app/controllers/menbers_controller.rb
@@ -2,10 +2,32 @@ class MenbersController < ApplicationController
   before_action :guest_check
 
   def like_list
-   favorites = Favorite.where(menber_id: current_menber.id).pluck(:post_id)
-   @favorite_posts = Post.find(favorites)
-   @favorite_posts = Kaminari.paginate_array(@favorite_posts).page(params[:page])
    @menber = current_menber
+   favorite = Favorite.where(menber_id: current_menber.id).pluck(:post_id)
+   @favorite = Post.where(id: favorite)
+   # @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(6)
+   if params[:search].blank? && params[:genre_id].blank?
+      @posts = Kaminari.paginate_array(@favorite).page(params[:page]).per(6)
+
+   elsif params[:search].present? && params[:genre_id].blank?
+      favorites = @favorite.where("title LIKE ?","%#{params[:search]}%")
+      @posts = Kaminari.paginate_array(favorites).page(params[:page]).per(6)
+
+   elsif params[:search].blank? && params[:genre_id].present?
+      target_genre_post_ids = Genre.find(params[:genre_id]).posts.ids #[1,2,3]
+      favorite_post_ids = @favorite.ids #[2,3,4]
+      @favorite = Post.where(id: (target_genre_post_ids & favorite_post_ids))
+      @posts = Kaminari.paginate_array(@favorite).page(params[:page]).per(6)
+
+   else
+      # search title
+      favorites_ids = @favorite.where("title LIKE ?","%#{params[:search]}%").ids
+      # search genre
+      target_genre_post_ids = Genre.find(params[:genre_id]).posts.ids #[1,2,3]
+      favorite_post_ids = @favorite.ids #[2,3,4]
+      @favorite = Post.where(id: ((target_genre_post_ids & favorite_post_ids) + favorites_ids).uniq)
+      @posts = Kaminari.paginate_array(@favorite).page(params[:page]).per(6)
+   end
   end
   def my_page
    @menber = current_menber
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,19 @@ class PostsController < ApplicationController
   before_action :authenticate_menber!
   def index
     if params[:search].blank? && params[:genre_id].blank?
-      @posts = Post.all.page(params[:page])
+      @posts = Post.all.order(id: "DESC").page(params[:page]).per(6)
+      
     elsif params[:search].present? && params[:genre_id].blank?
       @posts = Post.where("title LIKE ?","%#{params[:search]}%")
+      @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(6)
+      
     elsif params[:search].blank? && params[:genre_id].present?
       @posts = Genre.find(params[:genre_id]).posts
+      @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(6)
+      
     else
       @posts = Genre.find(params[:genre_id]).posts.where("title LIKE ?","%#{params[:search]}%")
+      @posts = Kaminari.paginate_array(@posts).page(params[:page]).per(6)
     end
     @menber = current_menber
   end

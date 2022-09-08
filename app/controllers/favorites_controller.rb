e2f99a225722f3a5bea2c748078223c4b8660c01
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-09-08 11:56:16 +0000

    [add/いいね]登録順序

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,16 @@ class FavoritesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     favorite = current_menber.favorites.new(post_id: post.id)
-    favorite.save
+    fav = favorite.reorder('favorites.created_at DESC')
+    fav.save
     redirect_back fallback_location: {action: "create"}
   end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_menber.favorites.find_by(post_id: post.id)
-    favorite.destroy
+    fav = favorite.reorder('favorites.created_at DESC')
+    fav.destroy
     redirect_back fallback_location: {action: "destroy"}
   end
 end

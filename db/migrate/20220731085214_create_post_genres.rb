a48df2a97d20a713f7e797bc750d2619a65257f7
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-09-17 10:47:25 +0000

    [migrate/oreign_key]

--- db/migrate/20220731085214_create_post_genres.rb
+++ db/migrate/20220731085214_create_post_genres.rb
@@ -1,10 +1,13 @@
 class CreatePostGenres < ActiveRecord::Migration[6.0]
   def change
     create_table :post_genres do |t|
-      t.references :post, null: false, foreign_key: true
-      t.references :genre, null: false, foreign_key: true
-
+      t.integer :post_id
+      t.integer :genre_id
+      # t.references :post, null: false, foreign_key: true
+      # t.references :genre, null: false, foreign_key: true
       t.timestamps
     end
+    add_foreign_key :post_genres, :posts
+    add_foreign_key :post_genres, :genres
   end
 end

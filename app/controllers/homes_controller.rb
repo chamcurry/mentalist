90992fd178893279f94b192ebf5fb14ab12a2412
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-09-19 04:45:40 +0000

    [update]投稿レイアウト

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
   before_action :authenticate_menber!,except: [:top]
-  
+
   def top
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC").page(params[:page]).per(6)
     @menber = current_menber
   end
 end

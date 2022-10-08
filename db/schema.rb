dde4ca96a72d84fb19e77955e3e42eddf3e9fa51
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-10-08 13:23:32 +0000

    [hidden]twitterAPI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_073619) do
+ActiveRecord::Schema.define(version: 2022_07_31_085214) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -79,8 +79,6 @@ ActiveRecord::Schema.define(version: 2022_10_08_073619) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "uid"
-    t.string "provider"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
